de5a32cfcd489289011307ee23feb9f0f3183f6e
Eunsong Lee <les911221@gmail.com>
2022-07-19 00:53:12 +0900

    first commit

---
+++ src/components/common/DatePicker.vue
@@ -0,0 +1,16 @@
+<template>
+  <input id="calender">
+</template>
+
+<script>
+export default {
+    mounted() {
+        /* eslint-disable */
+        $("#calender").datepicker();
+    }
+}
+</script>
+
+<style>
+
+</style>
